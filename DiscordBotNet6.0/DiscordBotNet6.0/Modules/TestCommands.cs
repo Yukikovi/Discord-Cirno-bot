166f0d94d959b17be0e3696bfff3516e82d98816
Yukikovi <62475400+Yukikovi@users.noreply.github.com>
2024-05-24 17:28:59 +0600

    delete trash

--- DiscordBotNet6.0/DiscordBotNet6.0/Modules/TestCommands.cs
+++ DiscordBotNet6.0/DiscordBotNet6.0/Modules/TestCommands.cs
@@ -19,48 +19,13 @@ namespace DiscordBotNet6._0.Modules
 {
     public class TestCommands : ModuleBase<SocketCommandContext>
     {
-        private OpenAIService gpt3
-        {
-            get
-            {
-                return new OpenAIService(new OpenAiOptions()
-                {
-                    ApiKey = configJsonAI.api
-                });
-            }
-        }
+      
         CommandHandler Handler { get; set; }
         public TestCommands(CommandHandler handler)
         {
             Handler = handler;
-            configJsonAI = JsonConvert.DeserializeObject<ConfigJson>(File.ReadAllText("Config\\config.json"));
+
         }
-        private ConfigJson configJsonAI { get; set; }
-        [Command("echo")]
-        public async Task TestAI(string input)
-        {
-            var completionResult = await gpt3.Completions.CreateCompletion(new CompletionCreateRequest()
-            {
-                Prompt = input,
-                Model = Models.CodeDavinciV2,
-                Temperature = 0.5F,
-                MaxTokens = 150
-            });
-            string outputQuestion = String.Empty;
-            if (completionResult.Successful)
-            {
-                foreach (var choice in completionResult.Choices)
-                {
-                    outputQuestion = choice.Text.ToString();
-                }
-                await ReplyAsync(outputQuestion);
-                //await RespondAsync(outputQuestion);
-            }
-            else
-            {
-                await ReplyAsync($"{completionResult.Error.Code}: {completionResult.Error.Message}");
-                Console.WriteLine($"{completionResult.Error.Code}: {completionResult.Error.Message}");
-            }
-        }
+
     }
 }
